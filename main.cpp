b4346676e50175f076652286cb3cfe8f8dadc04d
Alvineornela <127629007+Alvineornela@users.noreply.github.com>
2023-04-26 16:06:11 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,24 +2,23 @@
 using namespace std;
 
 int main() {
-  int n_sf= 1;
+ int n_F= 1;
     int add = 0;
-    int n_psf;
+    int n_precedenteF;
     int n1 = 0;
     int n;
     cin>> n;
     if(n>=2) {
         while (n1 < n) {
-            cout << n_sf << endl;
-            n_psf = n_sf;
-            n_sf = n_sf + add;
-            add = n_psf;
+            cout << n_F << endl;
+            n_precedenteF = n_F;
+            n_F = n_F + add;
+            add = n_precedenteF;
             n1++;
         }
-      } 
+    }
     else{
         cout<<"Errore!"<<endl;
     } 
-   
    return 0;
 }
