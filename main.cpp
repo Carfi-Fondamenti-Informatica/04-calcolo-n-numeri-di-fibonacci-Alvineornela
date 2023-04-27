cf6dd1c27041ef7c4dc1f25cbfe124ec3aadf02f
Alvineornela <127629007+Alvineornela@users.noreply.github.com>
2023-04-27 22:18:11 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,24 +1,22 @@
 #include <iostream>
 using namespace std;
-
 int main() {
- int n_F= 1;
-    int add = 0;
-    int n_precedenteF;
-    int n1 = 0;
-    int n;
-    cin>> n;
-    if(n>=2) {
-        while (n1 < n) {
-            cout << n_F << endl;
-            n_precedenteF = n_F;
-            n_F = n_F + add;
-            add = n_precedenteF;
-            n1++;
-        }
+int n,F,a=1,b=1;
+cin>>n;
+if(n<2){
+cout<<"errore!\n";
+}
+else {
+    int c = 0;
+    cout << a << endl;
+    cout << b << endl;
+    while (c < (n - 2)) {
+        c++;
+        F = a + b;
+        cout << F << endl;
+        a = b;
+        b = F;
     }
-    else{
-        cout<<"Errore!"<<endl;
-    } 
-   return 0;
+}
+return 0;
 }
